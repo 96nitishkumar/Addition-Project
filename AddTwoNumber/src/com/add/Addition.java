8c65eba317e6bf2899848962fd2bfcacae1e1eb5
Nitish kumar <nitishkkumar111@gamil.com>
2023-05-31 15:14:12 +0530

    changes done

--- AddTwoNumber/src/com/add/Addition.java
+++ AddTwoNumber/src/com/add/Addition.java
@@ -5,11 +5,11 @@ public class Addition {
 	public static void main(String[] args) {
 		
 	
-	int num1=20;
-	int num2=30;
+	int num1=30;
+	int num2=8;
 	int result;
-	result = num1+num2;
-	System.out.println("Addition of two number is :"+result);
+	result = num1*num2;
+	System.out.println("Multiplication of two number is :"+result);
 	}
 
 }
